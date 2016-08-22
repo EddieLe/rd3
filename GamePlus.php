3ddeec2eb251c6efceee447228bb3dedd90e8332
eddie <oakes1218@hotmail.com>
2016-08-22 08:56:44 +0000

    plus

--- GamePlus.php
+++ GamePlus.php
@@ -6,22 +6,24 @@ class Game
 
     function create()
     {
-        for ($i = 0; $i < 100; $i++) {
+        $time1 = microtime(true);
+
+        for ($i = 0; $i < 3000; $i++) {
             $rand[] = $i + 1;
         }
         shuffle($rand);
 
-        for ($i = 0; $i < 10; $i++) {
-            for ($j = 0; $j < 10; $j++) {
-                if ($rand[$i * 10 + $j] <= 40) {
-                    $rand[$i * 10 + $j] = "M";
+        for ($i = 0; $i < 50; $i++) {
+            for ($j = 0; $j < 60; $j++) {
+                if ($rand[$i * 60 + $j] <= 1200) {
+                    $rand[$i * 60 + $j] = "M";
                 }
-                $this->result[$i][$j] = $rand[$i * 10 + $j];
+                $this->result[$i][$j] = $rand[$i * 60 + $j];
             }
         }
 
-        for ($i = 0; $i < 10; $i++) {
-            for ($j = 0; $j < 10; $j++) {
+        for ($i = 0; $i < 50; $i++) {
+            for ($j = 0; $j < 60; $j++) {
                 if ($this->result[$i][$j] === "M") {
                     continue;
                 } else {
@@ -56,18 +58,30 @@ class Game
                 }
             }
         }
+        $time2 = microtime(true);
+        // for ($j = 0 ; $j < 50; $j++) {
+        //     echo "<tr>";
+        //     for ($i = 0 ; $i < 60; $i++) {
+        //         echo "<td>" . $this->result[$j][$i] . "</td>";
+        //     }
+        //     echo "</tr>";
+        // }
 
-        for ($j = 0 ; $j < 10; $j++) {
-            for ($i = 0 ; $i <= 10; $i++) {
+        for ($j = 0 ; $j < 50; $j++) {
+            for ($i = 0 ; $i <= 60; $i++) {
                 $re .= $this->result[$j][$i];
             }
-            if ($j != 9) {
+            if ($j != 49) {
                 $re .= "N";
             }
         }
+    // $time = $time2 - $time1;
+    // echo  $time ."<br>";
     echo $re;
     }
 }
 
 $g = new Game();
+// echo ' <table width="300" border="1"> ';
 $g->create();
+// echo ' </table> ';
